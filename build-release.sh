6062098a46feec1a03a47f5a57a938d31686f861
GhostTorment <jak@bracegirdle.io>
2025-08-06 14:10:59 +0100

    Added fix for relative paths

--- build-release.sh
+++ build-release.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
-set -e
+# cd into the directory where this script is located
+# shellcheck disable=SC2164
+cd "$(dirname "$0")"
 
-# Get the directory this script is in
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_ROOT="$(realpath "$SCRIPT_DIR/..")"
-
-# Build the project
-cd "$PROJECT_ROOT"
 cargo clean
-cargo build --release
+cargo build --profile release
 
-# Copy files using absolute paths based on the project root
-cp "$PROJECT_ROOT/.env" "$PROJECT_ROOT/package/etc/speedtest-statuspage/.env"
-cp "$PROJECT_ROOT/target/release/speedtest-statuspage" "$PROJECT_ROOT/package/usr/local/bin/speedtest-statuspage"
+cp .env package/etc/speedtest-statuspage/.env
+cp target/release/speedtest-statuspage package/usr/local/bin/speedtest-statuspage
