c03d0234b07766dafefaa909f976aed4295d7f87
GhostTorment <jak@bracegirdle.io>
2025-08-06 14:13:28 +0100

    Added fix for relative paths

--- build-release.sh
+++ build-release.sh
@@ -6,4 +6,4 @@ cargo clean
 cargo build --profile release
 
 cp .env package/etc/speedtest-statuspage/.env
-cp target/release/speedtest-statuspage package/usr/local/bin/speedtest-statuspage
+cp target/release/speedtest_statuspage package/usr/local/bin/speedtest-statuspage
